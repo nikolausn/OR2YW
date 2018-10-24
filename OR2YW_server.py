ade8d35f3c168bf7e0540867e14547df5c877cca
nikolausn <nikolaus.nova@gmail.com>
2018-10-23 20:07:42 -0500

    create a flask image encoding

--- OR2YW_server.py
+++ OR2YW_server.py
@@ -1,16 +1,20 @@
 from OR2YW import OR2YWGenerator
 from flask import Flask
+from flask import json
+from flask import request
 
 
 app = Flask(__name__)
 
-@app.route("/generate-image")
-def generate_image(operations):
-    operation_json = OR2YWGenerator.generate_yw_script(operations)
-    image_encoded = OR2YWGenerator.generate_yw_image(operation_json)
+@app.route("/generate-image",method=["POST"])
+def generate_image():
+    if request.headers['Content-Type'] == 'application/json':
+        operations = "JSON Message: " + json.dumps(request.json)
 
-    return "data:image/png;base64,{}".format(image_encoded)
+        operation_json = OR2YWGenerator.generate_yw_script(operations)
+        image_encoded = OR2YWGenerator.generate_yw_image(operation_json)
 
+        return "data:image/png;base64,{}".format(image_encoded)
 
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0")
